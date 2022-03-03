d7a2bd122349124ca4634aa50ad79ce393329ee0
Luc Vidal <l.vidal@rubika-edu.com>
2022-03-03 14:45:40 +0100

    SCPs scriptables + ressources couting and UI

--- ProjetSCP/Assets/Scripts/Ressources/RessourcesDisplay.cs
+++ ProjetSCP/Assets/Scripts/Ressources/RessourcesDisplay.cs
@@ -1,28 +1,69 @@
 ﻿using UnityEngine;
 using TMPro;
+using SCP.Ressources.Helper;
 
 namespace SCP.Ressources.Display
 {
     public class RessourcesDisplay : MonoBehaviour
     {
-        public RessourcesManager manager;
+        public static RessourcesManager manager;
+        public RessourcesManager nonStaticManager;
+
+        [Header("RessourceDisplays")]
+        public TextMeshProUGUI moneyDisplayObject;
+        public TextMeshProUGUI workerDisplayObject;
+        private static TextMeshProUGUI moneyDisplay;
+        private static TextMeshProUGUI workerDisplay;
 
         public void Start()
         {
+            //Temporary
+            var ressourcesManager = new RessourcesManager();
             //Get Manager and inject
             manager = Registry.Get<RessourcesManager>();
             manager.display = this;
+
+            moneyDisplay = moneyDisplayObject;
+            workerDisplay = workerDisplayObject;
+        }
+
+        //For Testing Only
+        public void Update()
+        {
+           if (Input.GetKeyDown(KeyCode.T))
+           {
+                manager.AddMoney(10);
+                UpdateMoneyDisplay();
+           } 
+
+           if (Input.GetKeyDown(KeyCode.Y))
+           {
+                manager.AddWorker();
+                UpdateHumanRessourcesDisplay();
+           }
+
+           if (Input.GetKeyDown(KeyCode.G))
+           {
+                manager.RemoveMoney(10);
+                UpdateMoneyDisplay();
+           }
+
+           if (Input.GetKeyDown(KeyCode.H))
+           {
+                manager.RemoveWorker();
+                UpdateHumanRessourcesDisplay();
+           }
         }
 
 
         public static void UpdateHumanRessourcesDisplay()
         {
-
+            workerDisplay.text = RessourcesHelper.GetAvailableWorkersCount(manager.HumanRessources) + " / " + manager.HumanRessources.Count;
         }
 
         public static void UpdateMoneyDisplay()
         {
-
+            moneyDisplay.text = manager.Money + " k";
         }
     }
 }
